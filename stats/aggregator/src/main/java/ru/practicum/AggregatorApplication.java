92c8a811077ba610679b682d3dc9ecc386d46cc3
Dmitry_iz <zzzufo@yandex.ru>
2026-01-31 16:59:14 +0300

    analyzer

--- stats/aggregator/src/main/java/ru/practicum/AggregatorApplication.java
+++ stats/aggregator/src/main/java/ru/practicum/AggregatorApplication.java
@@ -2,10 +2,13 @@ package ru.practicum;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import ru.practicum.config.KafkaSettingsConfig;
 
 @SpringBootApplication
 @EnableDiscoveryClient
+@EnableConfigurationProperties(KafkaSettingsConfig.class)
 public class AggregatorApplication {
     public static void main(String[] args) {
         SpringApplication.run(AggregatorApplication.class, args);
